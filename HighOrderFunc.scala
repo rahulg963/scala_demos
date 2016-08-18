c37828a73a673afc9ebae634bea35b0ead298c1e
rahulg93 <rahul.g@seomorpheus.com>
2016-08-18 10:59:31 +0530

    high order function

---
+++ HighOrderFunc.scala
@@ -0,0 +1,11 @@
+object HighOrderFunc{
+
+	def main(args: Array[String]) {
+		println( apply( layout, 10) )
+	}
+
+	def apply(f: Int => String, v: Int) = f(v)
+	
+	def layout[A](x: A) = "[" + x.toString() + "]"
+
+}
